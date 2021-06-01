48d041c757846a23dd9f3b33d30640836983d5c9
zsomborr <zsomborkincses@gmail.com>
2021-06-01 15:50:50 +0200

    fix: cross origin allowed, minor variable changes
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>

--- src/main/java/com/codecool/league/controller/RouteController.java
+++ src/main/java/com/codecool/league/controller/RouteController.java
@@ -2,10 +2,12 @@ package com.codecool.league.controller;
 
 import com.codecool.league.service.RiotApiService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class RouteController {
 
@@ -20,7 +22,7 @@ public class RouteController {
     public String getAllChampion() {
         return riotApiService.getAllChampion();
     }
-    
+
     @GetMapping("/free")
     public String getFreeChampions() {
         return riotApiService.getFreeChampions();
