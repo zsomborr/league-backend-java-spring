857bfd1aaff96cada6abe49a25972667e76c21b8
zsomborr <zsomborkincses@gmail.com>
2021-06-08 16:37:40 +0200

    refactor: riot user dao refactored
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/RiotUserDetailsDao.java
+++ src/main/java/com/codecool/league/dao/RiotUserDetailsDao.java
@@ -1,13 +1,12 @@
 package com.codecool.league.dao;
 
-import com.codecool.league.model.MatchHistoryModel;
-import com.codecool.league.model.RiotUserDetailModel;
-import com.codecool.league.model.matchResults.MatchResultModel;
+import com.codecool.league.model.riotUser.matchHistory.MatchHistoryModel;
+import com.codecool.league.model.riotUser.RiotUserDetailModel;
+import com.codecool.league.model.riotUser.matchResults.MatchResultModel;
 
-import java.util.List;
 
 public interface RiotUserDetailsDao {
     RiotUserDetailModel getUserInfo(String userName);
-    List<MatchHistoryModel> getMatchHistory(String accountId);
+    MatchHistoryModel getMatchHistory(String accountId);
     MatchResultModel getMatchResult(Long matchId);
 }
