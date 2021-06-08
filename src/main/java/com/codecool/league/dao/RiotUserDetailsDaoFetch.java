ff46f091802e79295fbd44c219e0c933b80fe8ae
zsomborr <zsomborkincses@gmail.com>
2021-06-08 16:37:40 +0200

    refactor: riot user dao refactored
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/RiotUserDetailsDaoFetch.java
+++ src/main/java/com/codecool/league/dao/RiotUserDetailsDaoFetch.java
@@ -1,15 +1,14 @@
 package com.codecool.league.dao;
 
-import com.codecool.league.model.MatchHistoryModel;
-import com.codecool.league.model.RiotUserDetailModel;
-import com.codecool.league.model.matchResults.MatchResultModel;
+import com.codecool.league.model.riotUser.matchHistory.MatchHistoryModel;
+import com.codecool.league.model.riotUser.RiotUserDetailModel;
+import com.codecool.league.model.riotUser.matchResults.MatchResultModel;
 import com.codecool.league.util.Util;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.List;
 
 public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao{
     @Override
@@ -25,12 +24,12 @@ public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao{
     }
 
     @Override
-    public List<MatchHistoryModel> getMatchHistory(String accountId) {
-        List<MatchHistoryModel> matchHistory = new ArrayList<>();
+    public MatchHistoryModel getMatchHistory(String accountId) {
+        MatchHistoryModel matchHistory = null;
         try {
-            String matchHistoryFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId);
-            Type matchHistoryModelType = new TypeToken<ArrayList<MatchHistoryModel>>(){}.getType();
-            matchHistory = new Gson().fromJson(matchHistoryFromApi, matchHistoryModelType);
+            String matchHistoryFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?endIndex=10");
+//            Type matchHistoryModelType = new TypeToken<ArrayList<MatchModel>>(){}.getType();
+            matchHistory = new Gson().fromJson(matchHistoryFromApi, MatchHistoryModel.class);
         } catch (Exception e) {
             e.printStackTrace();
         }
