0ce2abdd83bbb0ffe45933e6ae2ff85bcd3b8ba8
zsomborr <zsomborkincses@gmail.com>
2021-06-15 16:38:24 +0200

    feat: champion service works from repository
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>
    Co-authored-by: vidomark <vidomark@users.noreply.github.com>

--- src/main/java/com/codecool/league/service/ChampionsService.java
+++ src/main/java/com/codecool/league/service/ChampionsService.java
@@ -1,37 +1,35 @@
 package com.codecool.league.service;
 
-import com.codecool.league.dao.championsDao.ChampionsDao;
-import com.codecool.league.dao.freeChampionsDao.FreeChampionsDao;
+import com.codecool.league.dao.repository.ChampionsRepository;
+import com.codecool.league.model.champions.ChampionModel;
 import com.codecool.league.model.champions.ChampionsDataModel;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class ChampionsService {
 
-    ChampionsDao championsDao;
-    FreeChampionsDao freeChampionsDao;
-
     @Autowired
-    public ChampionsService(ChampionsDao championsDao, FreeChampionsDao freeChampionsDao) {
-        this.championsDao = championsDao;
-        this.freeChampionsDao = freeChampionsDao;
-    }
+    private ChampionsRepository championsRepository;
 
     public ChampionsDataModel getAllChampion() {
-        return mergeChampionsAndFreeData(championsDao.getAllChampion());
+        List<ChampionModel> championModels = championsRepository.findAll();
+        Map<String, ChampionModel> championsMap = championModels.stream()
+                .collect(Collectors.toMap(ChampionModel::getId, Function.identity()));
+        var championsDataModel = new ChampionsDataModel();
+        championsDataModel.setData(championsMap);
+        return championsDataModel;
     }
 
     public ChampionsDataModel getChampionsByTag(String tag) {
-        return mergeChampionsAndFreeData(championsDao.getChampionsByTag(tag));
-    }
-
-    private ChampionsDataModel mergeChampionsAndFreeData(ChampionsDataModel allChampion) {
-        List<Integer> freeChampionIds = freeChampionsDao.getFreeChampions().getFreeChampionIds();
-        allChampion.getData()
-                .forEach((key, champion) -> {
-                    champion.setFree(freeChampionIds.contains(Integer.parseInt(champion.getKey())));
-                });
-        return allChampion;
+        List<ChampionModel> championModels = championsRepository.findAllByTags(tag);
+        Map<String, ChampionModel> championsMap = championModels.stream()
+                .collect(Collectors.toMap(ChampionModel::getId, Function.identity()));
+        var championsDataModel = new ChampionsDataModel();
+        championsDataModel.setData(championsMap);
+        return championsDataModel;
     }
 }
