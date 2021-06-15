023df579eefd1b006eb949726c29fdfa041c8ee6
zsomborr <zsomborkincses@gmail.com>
2021-06-15 16:35:43 +0200

    refactor: url fields
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>
    Co-authored-by: vidomark <vidomark@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/fetch/freeChampionsDao/FreeChampionsDaoFetch.java
+++ src/main/java/com/codecool/league/dao/fetch/freeChampionsDao/FreeChampionsDaoFetch.java
@@ -1,15 +1,18 @@
-package com.codecool.league.dao.freeChampionsDao;
+package com.codecool.league.dao.fetch.freeChampionsDao;
 
 import com.codecool.league.model.freeChampions.FreeChampionsModel;
 import com.codecool.league.util.Util;
 import com.google.gson.Gson;
 
 public class FreeChampionsDaoFetch implements FreeChampionsDao {
+
+    private final String FREE_CHAMPIONS_ENDPOINT = "https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations";
+
     @Override
     public FreeChampionsModel getFreeChampions() {
         FreeChampionsModel freeChampions = null;
         try {
-            String freeChampionsFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/platform/v3/champion-rotations");
+            String freeChampionsFromApi = Util.getRiotApiJsonResponse(FREE_CHAMPIONS_ENDPOINT);
             freeChampions = new Gson().fromJson(freeChampionsFromApi, FreeChampionsModel.class);
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/codecool/league/dao/fetch/riotUserDetailsDao/RiotUserDetailsDaoFetch.java
+++ src/main/java/com/codecool/league/dao/fetch/riotUserDetailsDao/RiotUserDetailsDaoFetch.java
@@ -1,18 +1,21 @@
-package com.codecool.league.dao.riotUserDetailsDao;
+package com.codecool.league.dao.fetch.riotUserDetailsDao;
 
-import com.codecool.league.dao.riotUserDetailsDao.RiotUserDetailsDao;
-import com.codecool.league.model.riotUser.matchHistory.MatchHistoryModel;
 import com.codecool.league.model.riotUser.RiotUserDetailModel;
+import com.codecool.league.model.riotUser.matchHistory.MatchHistoryModel;
 import com.codecool.league.model.riotUser.matchResults.MatchResultModel;
 import com.codecool.league.util.Util;
 import com.google.gson.Gson;
 
 public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao {
+
+    private final String RIOTUSER_INFO_ENDPOINT = "https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/"; // + username
+    private final String MATCH_HISTORY_ENDPOINT = "https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/"; // + accoundId
+    private final String MATCH_RESULT_ENDPOINT = "https://eun1.api.riotgames.com/lol/match/v4/matches/"; // + matchId
     @Override
     public RiotUserDetailModel getUserInfo(String userName) {
         RiotUserDetailModel userDetail = null;
         try {
-            String userDetailFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/summoner/v4/summoners/by-name/" + userName);
+            String userDetailFromApi = Util.getRiotApiJsonResponse(RIOTUSER_INFO_ENDPOINT + userName);
             userDetail = new Gson().fromJson(userDetailFromApi, RiotUserDetailModel.class);
         } catch (Exception e) {
             e.printStackTrace();
@@ -24,8 +27,7 @@ public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao {
     public MatchHistoryModel getMatchHistory(String accountId) {
         MatchHistoryModel matchHistory = null;
         try {
-            String matchHistoryFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/match/v4/matchlists/by-account/" + accountId + "?endIndex=10");
-//            Type matchHistoryModelType = new TypeToken<ArrayList<MatchModel>>(){}.getType();
+            String matchHistoryFromApi = Util.getRiotApiJsonResponse(MATCH_HISTORY_ENDPOINT + accountId + "?endIndex=10");
             matchHistory = new Gson().fromJson(matchHistoryFromApi, MatchHistoryModel.class);
         } catch (Exception e) {
             e.printStackTrace();
@@ -37,7 +39,7 @@ public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao {
     public MatchResultModel getMatchResult(Long matchId) {
         MatchResultModel matchDetail = null;
         try {
-            String matchDetailFromApi = Util.getRiotApiJsonResponse("https://eun1.api.riotgames.com/lol/match/v4/matches/" + matchId);
+            String matchDetailFromApi = Util.getRiotApiJsonResponse(MATCH_RESULT_ENDPOINT + matchId);
             matchDetail = new Gson().fromJson(matchDetailFromApi, MatchResultModel.class);
         } catch (Exception e) {
             e.printStackTrace();
