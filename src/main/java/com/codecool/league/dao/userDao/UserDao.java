3cb148b0e44356c6204d2c9a8723c52eea442bb5
zsomborr <zsomborkincses@gmail.com>
2021-06-16 18:34:28 +0200

    refactor: deleted userdao
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/userDao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.codecool.league.dao.userDao;
-
-import com.codecool.league.model.user.UserModel;
-
-public interface UserDao {
-    Boolean getUser(UserModel userModel);
-    Boolean addUser(UserModel userModel);
-}
