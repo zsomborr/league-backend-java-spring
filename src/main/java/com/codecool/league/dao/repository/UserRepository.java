889804c04de6ebf727be0c6c32749e108b701c49
zsomborr <zsomborkincses@gmail.com>
2021-06-16 12:03:14 +0200

    feat: lombok annotations
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/repository/UserRepository.java
+++ src/main/java/com/codecool/league/dao/repository/UserRepository.java
@@ -3,5 +3,10 @@ package com.codecool.league.dao.repository;
 import com.codecool.league.model.user.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<UserModel, Long> {
+
+    UserModel findDistinctByEmail(String email);
+    UserModel findDistinctByEmailAndPassword(String email, byte[] password);
+
 }
