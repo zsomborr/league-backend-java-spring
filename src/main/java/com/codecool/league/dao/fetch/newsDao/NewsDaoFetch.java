8d97c7fdc67dbfadcb707b019bf126d6d759d994
zsomborr <zsomborkincses@gmail.com>
2021-06-17 12:01:50 +0200

    refactor: variable change
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/fetch/newsDao/NewsDaoFetch.java
+++ src/main/java/com/codecool/league/dao/fetch/newsDao/NewsDaoFetch.java
@@ -11,11 +11,13 @@ import java.util.List;
 
 public class NewsDaoFetch implements NewsDao {
 
+    private final static String NEWS_API = "https://api.hnpwa.com/v0/newest/";
+
     @Override
     public List<NewsModel> getNews(int pageNumber) {
         List<NewsModel> news = new ArrayList<>();
         try {
-            String newsFromApi = Util.getRiotApiJsonResponse("https://api.hnpwa.com/v0/newest/"+ pageNumber +".json");
+            String newsFromApi = Util.getRiotApiJsonResponse(NEWS_API + pageNumber +".json");
             Type newsModelType = new TypeToken<ArrayList<NewsModel>>(){}.getType();
             news = new Gson().fromJson(newsFromApi, newsModelType);
         } catch (Exception e) {
