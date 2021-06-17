a2babd2d7d9baa6e3b6de2b168d873686a30fad8
zsomborr <zsomborkincses@gmail.com>
2021-06-17 12:06:30 +0200

    feat: default models for exception
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

--- src/main/java/com/codecool/league/dao/fetch/riotUserDetailsDao/RiotUserDetailsDaoFetch.java
+++ src/main/java/com/codecool/league/dao/fetch/riotUserDetailsDao/RiotUserDetailsDaoFetch.java
@@ -2,9 +2,13 @@ package com.codecool.league.dao.fetch.riotUserDetailsDao;
 
 import com.codecool.league.model.riotUser.RiotUserDetailModel;
 import com.codecool.league.model.riotUser.matchHistory.MatchHistoryModel;
+import com.codecool.league.model.riotUser.matchHistory.MatchModel;
 import com.codecool.league.model.riotUser.matchResults.MatchResultModel;
+import com.codecool.league.model.riotUser.matchResults.Teams;
 import com.codecool.league.util.Util;
 import com.google.gson.Gson;
+
+import java.io.IOException;
 
 public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao {
 
@@ -14,37 +18,53 @@ public class RiotUserDetailsDaoFetch implements RiotUserDetailsDao {
 
     @Override
     public RiotUserDetailModel getUserInfo(String userName) {
-        RiotUserDetailModel userDetail = null;
         try {
             String userDetailFromApi = Util.getRiotApiJsonResponse(RIOTUSER_INFO_ENDPOINT + userName);
-            userDetail = new Gson().fromJson(userDetailFromApi, RiotUserDetailModel.class);
-        } catch (Exception e) {
-            e.printStackTrace();
+            return new Gson().fromJson(userDetailFromApi, RiotUserDetailModel.class);
+        } catch (IOException e) {
+            System.out.println("No such user");
         }
-        return userDetail;
+        return RiotUserDetailModel.builder()
+                .name("Not found")
+                .summonerLevel(0)
+                .build();
     }
 
     @Override
     public MatchHistoryModel getMatchHistory(String accountId) {
-        MatchHistoryModel matchHistory = null;
         try {
             String matchHistoryFromApi = Util.getRiotApiJsonResponse(MATCH_HISTORY_ENDPOINT + accountId + "?endIndex=10");
-            matchHistory = new Gson().fromJson(matchHistoryFromApi, MatchHistoryModel.class);
-        } catch (Exception e) {
-            e.printStackTrace();
+            return new Gson().fromJson(matchHistoryFromApi, MatchHistoryModel.class);
+        } catch (IOException e) {
+            System.out.println("No such match history");
         }
-        return matchHistory;
+        return MatchHistoryModel.builder()
+                .match(MatchModel.builder()
+                        .gameId(1)
+                        .champion(300000)
+                        .build())
+                .build();
     }
 
     @Override
     public MatchResultModel getMatchResult(Long matchId) {
-        MatchResultModel matchDetail = null;
         try {
             String matchDetailFromApi = Util.getRiotApiJsonResponse(MATCH_RESULT_ENDPOINT + matchId);
-            matchDetail = new Gson().fromJson(matchDetailFromApi, MatchResultModel.class);
-        } catch (Exception e) {
-            e.printStackTrace();
+            return new Gson().fromJson(matchDetailFromApi, MatchResultModel.class);
+        } catch (IOException e) {
+            System.out.println("No such match result");
         }
-        return matchDetail;
+        return MatchResultModel.builder()
+                .gameId(1)
+                .gameMode("No game")
+                .gameDuration(0)
+                .team(Teams.builder()
+                        .teamId(2)
+                        .win("no result").build())
+                .team(Teams.builder()
+                        .teamId(1)
+                        .win("no result")
+                        .build())
+                .build();
     }
 }
