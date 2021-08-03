9b0a085c0dd0ceb887653ae6e32aab14ed489909
zsomborr <zsomborkincses@gmail.com>
2021-08-03 14:47:17 +0200

    feat: added security classes
    Co-authored-by: bodisB <bodisB@users.noreply.github.com>
    Co-authored-by: ronairudi <ronairudi@users.noreply.github.com>

---
+++ src/main/java/com/codecool/league/security/JwtTokenServices.java
@@ -0,0 +1,92 @@
+package com.codecool.league.security;
+
+import com.codecool.league.model.user.Role;
+import io.jsonwebtoken.*;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Base64;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class JwtTokenServices {
+
+    @Value("${security.jwt.token.secret-key:secret}")
+    private String secretKey = "e455bfa8-7c7f-4b17-8bcd-aebffe258c6c";
+
+    @Value("${security.jwt.token.expire-length:3600000}")
+    private long validityInMilliseconds = 36000000; // 10 hours
+
+    private final String roleFieldName = "role";
+
+    @PostConstruct
+    protected void init() {
+        secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+    }
+
+    // Creates a JWT token
+    public String createToken(String username, Role role) {
+        // Add a custom field to the token
+        Claims claims = Jwts.claims().setSubject(username);
+        claims.put(roleFieldName, role);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + validityInMilliseconds);
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(validity)
+                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .compact();
+    }
+
+    public String getTokenFromRequest(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7, bearerToken.length());
+        }
+        return null;
+    }
+
+    // checks if the token is valid and not expired.
+    boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+            return !claims.getBody().getExpiration().before(new Date());
+        } catch (JwtException | IllegalArgumentException e) {
+            log.debug("JWT token invalid " + e);
+        }
+        return false;
+    }
+
+    /**
+     * Parses the username and roles from the token. Since the token is signed we can be sure its valid information.
+     * Note that it does not make a DB call to be super fast!
+     * This could result in returning false data (e.g. the user was deleted, but their token has not expired yet)
+     * To prevent errors because of this make sure to check the user in the database for more important calls!
+     */
+    Authentication parseUserFromTokenInfo(String token) throws UsernameNotFoundException {
+        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        String username = body.getSubject();
+        Role role = (Role) body.get(roleFieldName);
+        List<SimpleGrantedAuthority> authorities = new LinkedList<>();
+        authorities.add(new SimpleGrantedAuthority(role.name()));
+        return new UsernamePasswordAuthenticationToken(username, "", authorities);
+    }
+
+    public String getUsernameFromToken(String token) {
+        Claims body = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token).getBody();
+        return body.getSubject();
+    }
+}
